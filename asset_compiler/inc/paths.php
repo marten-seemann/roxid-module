eaa49a5fe42d10d833c67370ce3f6c70fc988952
Marten Seemann <martenseemann@gmail.com>
2019-01-19 10:56:11 +0700

    remove the asset compiler

--- asset_compiler/inc/paths.php
+++
@@ -1,6 +0,0 @@
-<?php
-// the paths of the LESS files that should be compiled relative to the roxid_mod/ directory
-$paths = array(
-  "src/css/theme_own.less" => "build/css/theme_own.css",
-  "src/css/bootstrap_own.less" => "build/css/bootstrap_own.css",
-  );
