4d55932c2cf24c83b176843831cfd3931f1c9d24
Marten Seemann <martenseemann@gmail.com>
2019-02-08 19:58:01 +0800

    simplify loading of basket item thumbnails

--- Application/Model/Basketitem.php
+++ Application/Model/Basketitem.php
@@ -2,24 +2,12 @@
 namespace SeemannIT\Roxid\Application\Model;
 
 class Basketitem extends Basketitem_parent {
-  protected function _setArticle($sProductId) {
-    parent::_setArticle($sProductId);
-    $oArticle = $this->getArticle(true, $sProductId);
-    $this->_sThumbnailUrl = $oArticle->getThumbnailUrl();
-  }
-
   /**
    * Returns article thumbnail picture url
    *
    * @return string
    */
-  public function getThumbnailUrl()
-  {
-      // icon url must be (re)loaded in case icon is not set or shop was switched between ssl/nonssl
-      if ($this->_sThumbnailUrl === null || $this->_blSsl != $this->getConfig()->isSsl()) {
-          $this->_sThumbnailUrl = $this->getArticle()->getThumbnailUrl();
-      }
-
-      return $this->_sThumbnailUrl;
+  public function getThumbnailUrl() {
+    return $this->getArticle()->getThumbnailUrl($this->_bSsl);
   }
 }
