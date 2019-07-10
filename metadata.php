2271b46eae72aed56d8ee942b19a0a2edd87edd7
Marten Seemann <martenseemann@gmail.com>
2019-07-10 19:09:44 +0700

    release v4.0.1

--- metadata.php
+++ metadata.php
@@ -19,7 +19,7 @@ $aModule = array(
         'de' => ''
     ],
     // 'thumbnail'    => 'picture.png',
-    'version'      => '4.0.0',
+    'version'      => '4.0.1',
     'author'       => 'Marten Seemann',
     'extend'       => [
         \OxidEsales\Eshop\Application\Component\BasketComponent::class => \SeemannIT\Roxid\Application\Component\BasketComponent::class,
